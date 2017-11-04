a7ffff0c0a74933cc3d96f52785218498ec2cd70
Idrees Kamal <ikamal97@gmail.com>
2017-11-04 13:59:59 -0500

    Implemented functionality for opening camera and taking a picture

--- app/src/main/java/com/example/idreeskamal/ladybug/MainActivity.java
+++ app/src/main/java/com/example/idreeskamal/ladybug/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.idreeskamal.ladybug;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +11,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.ImageView;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+    private Bitmap mImageBitmap;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,4 +57,30 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+//    private void handleSmallCameraPhoto(Intent intent) {
+//        Bundle extras = intent.getExtras();
+//        mImageBitmap = (Bitmap) extras.get("data");
+//        mImageView.setImageBitmap(mImageBitmap);
+//        //mVideoUri = null;
+//        mImageView.setVisibility(View.VISIBLE);
+//        //mVideoView.setVisibility(View.INVISIBLE);
+//    }
+
 }
